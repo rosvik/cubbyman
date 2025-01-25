3cf46d33f10002fa2e34f6abb68940bd0fb81cd6
Johannes Røsvik <j.rosvik@gmail.com>
2025-01-25 12:22:36 +0100

    refactor: extract get_credentials helper

--- src/commands/pull.rs
+++ src/commands/pull.rs
@@ -5,19 +5,24 @@ use std::env;
 
 pub async fn pull_image(connection: bollard::Docker, image: String) {
     println!("Pulling image {}", image);
+
+    let credentials = get_credentials();
     let options = CreateImageOptions::<String> {
         from_image: image,
-        ..Default::default()
-    };
-    let credentials = DockerCredentials {
-        username: Some(env::var("REGISTRY_USERNAME").unwrap()),
-        password: Some(env::var("REGISTRY_PASSWORD").unwrap()),
         ..Default::default()
     };
 
     let mut result = connection.create_image(Some(options), None, Some(credentials));
 
-    while let Some(next) = result.next().await {
-        println!("{:?}", next);
+    while let Some(Ok(create_image_info)) = result.next().await {
+        println!("{:?}", create_image_info);
+    }
+}
+
+fn get_credentials() -> DockerCredentials {
+    DockerCredentials {
+        username: Some(env::var("REGISTRY_USERNAME").unwrap()),
+        password: Some(env::var("REGISTRY_PASSWORD").unwrap()),
+        ..Default::default()
     }
 }
