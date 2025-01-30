e67c6408e58ce7b79ad18dd35c3e840661d0914a
Johannes Røsvik <j.rosvik@gmail.com>
2025-01-30 21:10:24 +0100

    feat: update webhook endpoints

--- src/serve.rs
+++ src/serve.rs
@@ -20,14 +20,17 @@ pub async fn serve(socket: bollard::Docker, config: Config) {
     let auth = middleware::from_fn_with_state(state.clone(), basic_authenticate);
     let app = Router::new()
         .route(
-            "/",
+            "/cubbyman",
             get(|| async { format!("{CRATE_NAME} v{CRATE_VERSION}") }),
         )
         .route(
-            "/v1",
+            "/cubbyman/v1",
             get(|| async { "Authenticated" }).route_layer(auth.clone()),
         )
-        .route("/v1/reload", post(reload).route_layer(auth.clone()))
+        .route(
+            "/cubbyman/v1/reload",
+            post(reload).route_layer(auth.clone()),
+        )
         .with_state(state.clone());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8600").await.unwrap();
