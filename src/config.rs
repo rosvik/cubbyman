faac54a4e616bfb8c30e1f2d0765e7a539de9d26
Johannes Røsvik <j.rosvik@gmail.com>
2026-04-25 13:42:56 +0200

    feat: support single key secrets

--- src/config.rs
+++ src/config.rs
@@ -107,12 +107,16 @@ pub struct Secret {
 }
 
 impl Secret {
-    pub fn from_string(string: &str) -> Self {
-        let (dotenv_key, container_env_key) = string.split_once(':').unwrap();
-        Self {
+    pub fn from_string(string: &str) -> Result<Self, std::io::ErrorKind> {
+        let string = string.trim();
+        if string.is_empty() {
+            return Err(std::io::ErrorKind::InvalidInput);
+        }
+        let (dotenv_key, container_env_key) = string.split_once(':').unwrap_or((string, string));
+        Ok(Self {
             dotenv_key: dotenv_key.to_string(),
             container_env_key: container_env_key.to_string(),
-        }
+        })
     }
 }
 
@@ -121,7 +125,18 @@ where
     D: Deserializer<'de>,
 {
     let secrets: Option<Vec<String>> = Deserialize::deserialize(deserializer)?;
-    Ok(secrets.map(|secrets| secrets.iter().map(|s| Secret::from_string(s)).collect()))
+    match secrets {
+        Some(secrets) => {
+            let secrets: Vec<Secret> = secrets
+                .iter()
+                .map(|s| Secret::from_string(s))
+                .filter(|s| s.is_ok())
+                .flatten()
+                .collect();
+            Ok(Some(secrets))
+        }
+        None => Ok(None),
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
